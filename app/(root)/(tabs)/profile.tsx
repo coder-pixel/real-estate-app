15b8eb35b89969f660b16e484e02216f9185e385
sauvik <sauvik@logic-square.com>
2025-02-22 13:12:06 +0530

    setting basic routes

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Profie = () => {
+  return (
+    <View>
+      <Text>Profie</Text>
+    </View>
+  );
+};
+
+export default Profie;
