96919244255fb638c69b9146635c775adb65ad0c
sauvik <sauvik@logic-square.com>
2025-03-01 22:39:01 +0530

    feat: added bottom tabs navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <Text className="font-bold my-10 font-rubik text-3xl">
         Welcome to ReEstate App
       </Text>
-      <Link href="/sign-in">Sign In</Link>
+      {/* <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore </Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property 1</Link>
+      <Link href="/properties/1">Property 1</Link> */}
     </View>
   );
 }
